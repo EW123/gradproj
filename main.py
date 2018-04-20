ed8b3b010d6480484121352ad8cee2232d373ebe
EW123 <253750329@qq.com>
2018-04-20 11:22:12 +0000

    train

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 from torch.autograd import Variable
 
@@ -11,10 +12,23 @@ valdir='/input/pl365/places365_standard/val'
 
 class Net(nn.Module):
 	def __init__(self):
-		pass
+		super(Net,self).__init__()
+		self.conv1=nn.Conv2d(3,6,5)
+		self.pool=nn.MaxPool2d(2,2)
+		self.conv2=nn.Conv2d(6,16,5)
+		self.fc1=nn.Linear(16*54*54,14010)
+		self.fc2=nn.Linear(14010,9807)
+		self.fc3=nn.Linear(9807,365)
+
 
 	def forward(self,x):
-		pass
+		x=self.pool(F.relu(self.conv1(x)))
+		x=self.poll(F.relu(self.conv2(x)))
+		x=x.view(-1,16*54*54)
+		x=F.relu(self.fc1(x))
+		x=F.relu(self.fc2(x))
+		x=self.fc2(x)
+		return x
 
 
 def main():
@@ -41,6 +55,22 @@ def main():
 					      shuffle=False,num_workers=2)
 
 	net=Net()
+
+	criterion=nn.CrossEntropyLoss()
+	optimizer=optim.SGD(net.parameters(),lr=0.001,momentum=0.9)
+
+	for epoch in range(2):
+
+		running_loss=0.0
+		for i,data in enumerate(trainloader,0):
+
+			inputs,labels=data
+
+			inputs,labels=Variable(inputs),Variables(labels)
+
+			optimizer.zero_grad()
+			
+			
 
 if __name__=='__main__':
 	main()
