85b308ca378a8a29978be185915e00c13d728d5d
EW123 <253750329@qq.com>
2018-04-27 03:53:56 +0000

    resnet

--- main.py
+++ main.py
@@ -4,31 +4,12 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision.models as models
 
 from torch.autograd import Variable
 
 traindir='/input/pl365/places365_standard/train'
 valdir='/input/pl365/places365_standard/val'
-
-class Net(nn.Module):
-	def __init__(self):
-		super(Net,self).__init__()
-		self.conv1=nn.Conv2d(3,6,5)
-		self.pool=nn.MaxPool2d(2,2)
-		self.conv2=nn.Conv2d(6,16,5)
-		self.fc1=nn.Linear(16*54*54,14010)
-		self.fc2=nn.Linear(14010,9807)
-		self.fc3=nn.Linear(9807,365)
-
-
-	def forward(self,x):
-		x=self.pool(F.relu(self.conv1(x)))
-		x=self.poll(F.relu(self.conv2(x)))
-		x=x.view(-1,16*54*54)
-		x=F.relu(self.fc1(x))
-		x=F.relu(self.fc2(x))
-		x=self.fc2(x)
-		return x
 
 
 def main():
@@ -37,26 +18,26 @@ def main():
                                       std=[0.229,0.224,0.225])
 
 	trainset=torchvision.datasets.ImageFolder(traindir,transform=transforms.Compose(
-                [normalize,
-                 transforms.RandomResizedCrop(224),
-                 transforms.RandomHorizontalFlip(),
-		 transforms.ToTensor(),]))
+                [transforms.RandomResizedCrop(224),
+            	transforms.RandomHorizontalFlip(),
+            	transforms.ToTensor(),
+            	normalize,]))
 	print("prepare trainloader")
 	trainloader=torch.utils.data.DataLoader(trainset,batch_size=4,
 						shuffle=True,num_workers=2)
 
 	valset=torchvision.datasets.ImageFolder(valdir,transforms.Compose([
-				transforms.Resize(256),
-				transforms.CenterCrop(224),
-				normalize,
-				transforms.ToTensor(),]))
+				            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,]))
 	print("prepare valloader")
 	valloader=torch.utils.data.DataLoader(valset,batch_size=4,
 					      shuffle=False,num_workers=2)
 
-	net=Net()
+	net=models.resnet18().cuda()
 
-	criterion=nn.CrossEntropyLoss()
+	criterion=nn.CrossEntropyLoss().cuda()
 	optimizer=optim.SGD(net.parameters(),lr=0.001,momentum=0.9)
 
 	for epoch in range(2):
@@ -66,8 +47,10 @@ def main():
 		for i,data in enumerate(trainloader,0):
 
 			inputs,labels=data
+			inputs=inputs.cuda(async=True)
+			labels=labels.cuda(async=True)
 
-			inputs,labels=Variable(inputs),Variables(labels)
+			inputs,labels=Variable(inputs),Variable(labels)
 
 			optimizer.zero_grad()
 
@@ -81,7 +64,7 @@ def main():
 			
 			running_loss+=loss.data[0]
 			if i%2000==0:
-				print("[%d %d] loss:%.3f" %epoch %i %running_loss)
+				print("[%d %d] loss:%.3f" %(epoch,i,running_loss))
 
 			
 
